640e5687f39859522204f29332a6f91978a930c1
rohautb <rohautb@127.0.0.1>
2016-05-18 10:13:02 +0200

    Sauvegardes

---
+++ Renju/src/Vue/Sauvegarde.java
@@ -0,0 +1,65 @@
+package Vue;
+
+import Controleur.*;
+import java.io.*;
+public class Sauvegarde {
+	File[] slot = new File[10];
+	
+	@SuppressWarnings("null")
+	public Sauvegarde(){
+		String chemin = System.getProperty("user.dir"); //+ "/saves"
+		File[] files = new File(chemin).listFiles();
+		String name;
+		char[] numero;
+		int num = 1;
+		for (File f : files){
+			numero = null;
+			name = f.getName();
+			name.getChars(0, 1, numero, 0);
+			num = (int)numero[0];
+			if( name.endsWith(".ser") && num >=0 && num <= 9 ){
+				slot[num] = f;
+			}
+		}
+		
+	}
+	/*
+	 * 
+	 * Sauvegarder la partie p au slot i 
+	 * 
+	 */
+	public void Sauvegarder(Partie p, int i){
+		try{
+			File f = new File(i+1 + "." + p.getJoueur1().getNom() + "-vs-" + p.getJoueur2().getNom() + ".ser");
+			FileOutputStream fos = new FileOutputStream(i + ". " + p.getJoueur1().getNom() + " vs " + p.getJoueur2().getNom() + ".ser");
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(p);
+			oos.close();
+			slot[i] =  f;
+		}
+		catch(IOException|NullPointerException e){
+			System.out.println("Erreur lors de la sauvegarde du fichier");
+		}
+	}
+	
+	/*
+	 * 
+	 * Charger la partie au slot i
+	 * 
+	 */
+	public Partie Charger(int i){
+		try{
+			FileInputStream fis = new FileInputStream(slot[i].getName());
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			Object p = ois.readObject();
+			ois.close();
+			if( p instanceof Partie ){
+				return (Partie)p;
+			}
+		}
+		catch(IOException|NullPointerException|ClassNotFoundException e){
+			System.out.println("Erreur lors du chargement du fichier");
+		}
+		return null;
+	}
+}
