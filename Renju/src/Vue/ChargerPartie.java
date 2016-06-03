d42080457b44171d2ee6feebef68935c2a3b7b15
rohautb <rohautb@127.0.0.1>
2016-06-03 10:32:13 +0200

    Mise à jour IHM du menu chargement

--- Renju/src/Vue/ChargerPartie.java
+++ Renju/src/Vue/ChargerPartie.java
@@ -12,6 +12,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 
 /**
  *
@@ -21,9 +27,11 @@ public class ChargerPartie extends JPanel {
 
     ButtonGroup grpPartie = new ButtonGroup();
     Fenetre fenetre;
+    Accueil acc;
 
     public ChargerPartie(Fenetre f) {
         this.fenetre = f;
+        this.acc = new Accueil(fenetre);
         RefreshSaves();
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
     }
@@ -41,17 +49,60 @@ public class ChargerPartie extends JPanel {
                 JButton rb = new JButton(val);
                 rb.addActionListener(new ChargerPartieListener(fenetre, i));
                 grpPartie.add(rb);
-                this.add(rb);
+                this.addButton(rb);
                 vide++;
             }
         }
         if (vide == 0) {
-            JLabel warning = new JLabel("Aucune sauvegarde disponible");
-            this.add(warning);
+            //petite triche pour avoir le texte et le bouton retour centrés
+            JButton btnVide = new JButton("Aucune sauvegarde disponible");
+            this.addButton(btnVide);
+            btnVide.addMouseListener(new java.awt.event.MouseAdapter() {
+                public void mouseEntered(java.awt.event.MouseEvent evt) {
+                    setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+                }
+            });
+            btnVide.setOpaque(false);
+            btnVide.setForeground(Color.white);
         }
-        JButton btnAnnuler = new JButton("Annuler");
+        JButton btnAnnuler = new JButton("Retour à l'accueil");
         btnAnnuler.addActionListener(new AnnulerVersAccueilListener(fenetre));
-        this.add(btnAnnuler);
+        this.addButton(btnAnnuler);
+    }
 
+    @Override
+    protected void paintComponent(Graphics g) {
+        Dimension d = getParent().getSize();
+        try {
+            BufferedImage img = ImageIO.read(new File("src/Ressources/acceuil.jpg"));
+            g.drawImage(img, 0, 0, d.width, d.height, null);
+        } catch (IOException ex) {
+            Logger.getLogger(AireDeJeu.class.getName()).log(Level.SEVERE, null, ex);
+            g.setColor(Color.GRAY);
+            g.fillRect(0, 0, d.width, d.height);
+        }
+    }
+
+    private void addButton(final JButton button) {
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        button.setFocusPainted(false);
+        button.setBackground(new Color(247, 128, 104));
+        button.setForeground(Color.white);
+        button.setBorderPainted(false);
+        button.setSelected(false);
+        button.setFont(new Font("Calibri", Font.BOLD, 25));
+        button.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                button.setBackground(new Color(252, 148, 119));
+                setCursor(new Cursor(Cursor.HAND_CURSOR));
+            }
+
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                button.setBackground(new Color(247, 128, 104));
+                setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+            }
+        });
+        this.add(button);
+        this.add(Box.createVerticalGlue());
     }
 }
